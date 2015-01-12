57023c8f4f491616af83a5c9270bbb2788b1d13e
Tony DiPasquale <tony@thoughtbot.com>
2015-01-12 18:54:10 -0500

    Initial Commit

---
+++ Naught/Naught.h
@@ -0,0 +1,19 @@
+//
+//  Naught.h
+//  Naught
+//
+//  Created by Tony DiPasquale on 1/12/15.
+//  Copyright (c) 2015 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Naught.
+FOUNDATION_EXPORT double NaughtVersionNumber;
+
+//! Project version string for Naught.
+FOUNDATION_EXPORT const unsigned char NaughtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Naught/PublicHeader.h>
+
+
